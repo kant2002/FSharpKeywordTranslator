0242ffd4ca2309c3012906649e9e2472deee4b26
Andrii Kurdiumov <kant2002@gmail.com>
2023-04-07 15:24:33 +0600

    Add more translations

---
+++ FSharpKeywordTranslator/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace FSharpKeywordTranslator;
+
+public static class Extensions
+{
+    public static string ToUpperCase(this string value)
+    {
+        return value[0].ToString().ToUpper() + value[1..];
+    }
+}
--- FSharpKeywordTranslator/VariablesObj.cs
+++ FSharpKeywordTranslator/VariablesObj.cs
@@ -4,4 +4,23 @@ public class VariablesObj
 {
     public string Name { get; set; }
     public string Age { get; set; }
+    public string Sum { get; set; }
+    public string Fibonacchi { get; set; }
+    public string Increment { get; set; }
+    public string Result1 { get; set; }
+    public string Result2 { get; set; }
+    public string Apply1 { get; set; }
+    public string Filter1 { get; set; }
+    public string Three { get; set; }
+    public string Color { get; set; }
+    public string Red { get; set; }
+    public string Green { get; set; }
+    public string Blue { get; set; }
+    public string PrintColorName { get; set; }
+    public string PersonName { get; set; }
+    public string FirstOnly { get; set; }
+    public string LastOnly { get; set; }
+    public string FirstLast { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
