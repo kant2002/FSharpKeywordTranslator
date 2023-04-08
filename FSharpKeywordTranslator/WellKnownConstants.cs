73fb9b52041e8af473882b8086f5c19a93566aeb
Andrii Kurdiumov <kant2002@gmail.com>
2023-04-08 16:49:58 +0600

    Add Polish

--- FSharpKeywordTranslator/WellKnownConstants.cs
+++ FSharpKeywordTranslator/WellKnownConstants.cs
@@ -2,5 +2,5 @@
 
 public static class WellKnownConstants
 {
-    public static readonly string[] Languages = new[] { "en", "ua", "tr", "kz", "es", "de", "it", "ru" };
+    public static readonly string[] Languages = new[] { "en", "ua", "tr", "kz", "es", "de", "it", "pl", "ru" };
 }
