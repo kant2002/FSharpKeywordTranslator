246a01b3b02db2be19499010f53162d8850c361f
Andrii Kurdiumov <kant2002@gmail.com>
2026-07-17 17:32:52 +0200

    Test fsharp.ps1 part of script works for main .NET 11

--- fsharp.ps1
+++ fsharp.ps1
@@ -4,13 +4,24 @@ param (
     [string]$Language = $(throw "-Language is required.")
 )
 
-git -C "$FSharpRepo" checkout -- .
-git -C "$FSharpRepo" checkout main
-Remove-Item –path  "$FSharpRepo\artifacts\" –Recurse
-dotnet run --project FSharpKeywordTranslator.Cli --  fsharp --lang $Language | git -C "$FSharpRepo" apply
+$ErrorActionPreference = 'Stop'
+$PrepareRepo = $true
+if ($PrepareRepo) {
+    git -C "$FSharpRepo" checkout -- .
+    git -C "$FSharpRepo" checkout main
+    Remove-Item –path  "$FSharpRepo\artifacts\" –Recurse -Force
+    dotnet run --project FSharpKeywordTranslator.Cli --  fsharp --lang $Language | git -C "$FSharpRepo" apply
+}
 try {
     pushd $FSharpRepo
-    .\Build.cmd -pack -ci /p:OfficialBuild=true /p:PublishWindowsPdb=false /p:VisualStudioDropName=dummy -c Release
+    $PackCustomFSharp = $true
+    if ($PackCustomFSharp) {
+        .\Build.cmd -pack -ci /p:OfficialBuild=true /p:PublishWindowsPdb=false /p:VisualStudioDropName=dummy -c Release
+    }
+
+    mkdir "$OutputStorage\$Language\artifacts\bin\fsc\Release" -Force
+    mkdir "$OutputStorage\$Language\artifacts\bin\fsi\Release" -Force
+    mkdir "$OutputStorage\$Language\artifacts\VSSetup\Release" -Force
     Copy-Item "$FSharpRepo\artifacts\bin\fsc\Release\*" -Destination "$OutputStorage\$Language\artifacts\bin\fsc\Release" -Recurse
     Copy-Item "$FSharpRepo\artifacts\bin\fsi\Release\*" -Destination "$OutputStorage\$Language\artifacts\bin\fsi\Release" -Recurse
     Copy-Item "$FSharpRepo\artifacts\VSSetup\Release\*" -Destination "$OutputStorage\$Language\artifacts\VSSetup\Release" -Recurse
